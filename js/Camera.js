39f148dd38087d8989943c976b7be529275db6d5
Nick <pattison.nick@gmail.com>
2018-03-18 23:57:26 -0400

    Added camera smoothing, began NPC class

--- js/Camera.js
+++ js/Camera.js
@@ -1,14 +1,21 @@
 function Camera(){
     this.x=0;
     this.y=0;
+    this.tx=0;
+    this.ty=0;
     this.update=function(dt, target){
-        if(target.rect){
-            this.x=target.rect.x;
-            this.y=target.rect.y;
-        } else {
-            this.x=target.x||this.x;
-            this.y=target.y||this.y;
+        if(target){
+            if(target.rect){
+                this.tx=target.rect.x;
+                this.ty=target.rect.y;
+            } else {
+                this.tx=target.x||this.tx;
+                this.ty=target.y||this.ty;
+            }
         }
+        const speed=5;
+        this.x+=(this.tx-this.x)*dt*speed;
+        this.y+=(this.ty-this.y)*dt*speed;
     };
     this.drawStart=function(gfx){
         gfx.translate(game.width/2-this.x, game.height/2-this.y);
---
+++ js/NPC.js
@@ -0,0 +1,10 @@
+function NPC(){
+    this.rect=new Rect(100, 100, 25, 25);
+    
+    this.update=function(dt){
+        
+    };
+    this.draw=function(gfx){
+        
+    };
+}
