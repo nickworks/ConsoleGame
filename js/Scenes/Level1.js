30d2a6670423dee9c206d2e207bc54adfb5634da
Nick <pattison.nick@gmail.com>
2018-03-23 19:55:30 -0400

    Level serialization now works

--- js/Scenes/Level1.js
+++ js/Scenes/Level1.js
@@ -1,4 +1,4 @@
-function spawnObjects(d){
+function deserialize(d){
     const objs={
         player:null,
         platforms:[],
@@ -20,28 +20,34 @@ function spawnObjects(d){
     });
     return objs;
 }
+function serialize(){
+    let res="[";
+    const f=(t,a)=>{
+        res+="{t:"+t.name+",d:[";
+        a.forEach(i=>{
+            const r=(i.rect||i.pawn.rect).raw();
+            res+="{x:"+r.x;
+            res+=",y:"+r.y;
+            res+=",w:"+r.w;
+            res+=",h:"+r.h;
+            res+="},";
+        });
+        res+="]},";
+    };
+    f(Player,[scene.player]);
+    f(Platform,scene.platforms);
+    f(NPC,scene.npcs);
+    f(Enemy,scene.enemies);
+    f(Door,scene.doors);
+    res+="]";
+    return res;
+}
 
 function Level1(n){
     
-    const d = [
-        {t:Player,d:[{x:0,y:0}]},
-        {t:Platform,d:[
-            {x:-1000,y:200,w:1100,h:100},
-            {x:  100,y:150,w: 100,h:100},
-            {x:  200,y:200,w: 100,h:100},
-        ]},
-        {t:NPC,d:[
-            {x:100,y:100}
-        ]},
-        {t:Enemy,d:[
-            {x:100,y:100}
-        ]},
-        {t:Door,d:[
-            {x:300,y:100}
-        ]}
-    ];
+    const d = [{t:Player,d:[{x:0,y:175,w:25,h:25},]},{t:Platform,d:[{x:-1000,y:200,w:1100,h:100},{x:100,y:150,w:100,h:100},{x:200,y:200,w:100,h:100},]},{t:NPC,d:[{x:100,y:100,w:25,h:25},]},{t:Enemy,d:[{x:100,y:125,w:25,h:25},]},{t:Door,d:[{x:300,y:100,w:25,h:100},]},];
     
-    const objs=spawnObjects(d);
+    const objs=deserialize(d);
     
     //make internal references here...
     
