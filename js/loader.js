3e417c6e7ef5791e7ad704ecb0a9a7060104c3fa
Nick <pattison.nick@gmail.com>
2018-03-22 23:09:40 -0400

    Organized scripts into folders

---
+++ js/loader.js
@@ -0,0 +1,32 @@
+((a,c)=>{
+    let loaded=0;
+    a.forEach((u)=>{
+        const s=document.createElement('script');
+        s.addEventListener("load", ()=>{if(++loaded==a.length)c();});
+        s.src=u; document.head.appendChild(s);
+    });
+})([
+    'js/UI/Button.js',
+    'js/UI/Menu.js',
+    'js/UI/TextField.js',
+    'js/UI/Dialog.js',
+    'js/UI/Pause.js',
+    'js/GameObjects/Camera.js',
+    'js/GameObjects/NPC.js',
+    'js/GameObjects/Platform.js',
+    'js/GameObjects/Player.js',
+    'js/GameObjects/Door.js',
+    'js/GameObjects/Bullet.js',
+    'js/core/Rect.js',
+    'js/core/ScenePlay.js',
+    'js/core/SceneTitle.js',
+    'js/core/Sprite.js',
+    'js/core/editor.js',
+    'js/core/keyboard.js',
+    'js/core/mouse.js',
+    'js/core/console.js',
+    'js/core/levels.js',
+    'js/core/game.js'
+],()=>{
+    game.begin("myCanvas");
+});
